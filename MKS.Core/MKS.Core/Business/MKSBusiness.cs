4044801444558d99e8a5fea626acf83385804534
stephane.dufour@gbsens.com <stephane.dufour@gbsens.com>
2016-03-12 07:40:07 -0500

    Ajout d'un MKSBusiness pour faciliter l'utilisation d'injection de dépendance.

---
+++ MKS.Core/MKS.Core/Business/MKSBusiness.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MKS.Core.Business;
+using MKS.Core.Model;
+
+// Rend accessible en instance la création de Business.
+namespace MKS.Core.Business
+{
+
+    public class MKSBusiness<TObject>:Business<TObject> {  }
+    public class MKSBusiness<TObject, TKey>:Business<TObject,TKey> where TKey : IKey { }
+    public class MKSBusiness<TObject, TResult, TSearch> : Business<TObject, TResult, TSearch> where TSearch:ISearch { }
+    public class MKSBusiness<TObject, TResult, TSearch, TKey> : Business<TObject, TResult, TSearch, TKey> where TSearch:ISearch where TKey:IKey { }
+
+}
