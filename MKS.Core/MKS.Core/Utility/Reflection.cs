2a3f4dc4133678c7960b8dc732069548faf13632
stephane.dufour@gbsens.com <stephane.dufour@gbsens.com>
2016-03-21 21:33:12 -0400

    Ajout du fichier manquant dans le repository

---
+++ MKS.Core/MKS.Core/Utility/Reflection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKS.Core.Utility
+{
+    public static class Reflection
+    {
+
+        public static IEnumerable<KeyValuePair<string, T>> PropertiesOfType<T>(object obj)
+        {
+            return from p in obj.GetType().GetProperties() 
+                   where p.PropertyType == typeof(T)
+                   select new KeyValuePair<string, T>(p.Name, (T)p.GetValue(obj));
+        }
+    }
+}
