82c7effc450158e761e37a41900886c0f86d75f7
stephane.dufour@gbsens.com <stephane.dufour@gbsens.com>
2016-04-07 19:54:16 -0400

    Ajout de commentaire

--- MKS.Core/MKS.Core/Utility/Reflection.cs
+++ MKS.Core/MKS.Core/Utility/Reflection.cs
@@ -8,7 +8,12 @@ namespace MKS.Core.Utility
 {
     public static class Reflection
     {
-
+        /// <summary>
+        /// Lis les propriété d'un object
+        /// </summary>
+        /// <typeparam name="T">Type de l'objet</typeparam>
+        /// <param name="obj">Instance de l'objet</param>
+        /// <returns>Liste de propiétés</returns>
         public static IEnumerable<KeyValuePair<string, T>> PropertiesOfType<T>(object obj)
         {
             return from p in obj.GetType().GetProperties() 
