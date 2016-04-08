e21c4aa6a5d701be23c4f507d8e7256c66b7f5c1
Stéphane Dufour <stephane.dufour@gbsens.com>
2016-04-07 21:16:49 -0400

    Page de base pour Application Web avec bootstrap

---
+++ MKS.Web.BootStrap/Utile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace MKS.Web.BootStrap
+{
+    public static class GridView
+    {
+        public static void AssignColumName(DataControlFieldCollection col, string name, object value )
+        {
+            for (int i = 0; i < col.Count; i++)
+            {
+                if(col[i].HeaderText==name)
+                {
+                     col[i].HeaderText=value as string;
+                }
+            }
+       
+        }
+        public static List<ListItem> BindPager(int totalRecordCount, int currentPageIndex, int pageSize)
+        {
+            double getPageCount = (double)((decimal)totalRecordCount / (decimal)pageSize);
+            int pageCount = (int)Math.Ceiling(getPageCount);
+            List<ListItem> pages = new List<ListItem>();
+            if (pageCount > 1)
+            {
+                pages.Add(new ListItem("<<", "1", currentPageIndex > 1));
+                for (int i = 1; i <= pageCount; i++)
+                {
+                    pages.Add(new ListItem(i.ToString(), i.ToString(), i != currentPageIndex + 1));
+                }
+                pages.Add(new ListItem(">>", pageCount.ToString(), currentPageIndex < pageCount - 1));
+            }
+
+            return pages;
+        }
+    }
+
+}
